63bd60e5861b21bfe0c8aa67f7c6125259bdbba7
Antonello <farruko7299@gmail.com>
2023-02-05 13:36:10 -0500

    Se añadio Lista de Usuarios y Validaciones

--- commands/cuenta.js
+++ commands/cuenta.js
@@ -3,12 +3,28 @@ module.exports = async (bot, chatId, typeAccount) => {
     try {
         console.log(typeAccount);
         const cuenta = await getAccount(typeAccount);
-        bot.sendMessage(chatId, `Tu cuenta es:\n${cuenta}`);
+        if(cuenta === "File Not Found") {
+            await bot.sendMessage(chatId, `
+▶️ ═══════════ LIGHBOT ═══════════
+❌ Plataforma: ${typeAccount.toUpperCase()}
+⛔️ Mensaje: Tipo de Cuenta no Existente
+❗️ Bot: @AntonelloBot
+                    `);
+            return;
+        }
+        if(!cuenta || cuenta === "") return;
+        await bot.sendMessage(chatId, `
+▶️ ═══════════ LIGHBOT ═══════════
+✅ Plataforma: ${typeAccount.toUpperCase()}
+✅ Cuenta: ${cuenta}
+❗️ Bot: @AntonelloBot
+        `);
     } catch (error) {
         console.log(error);
     }
 };
 const getAccount = async (path) => {
+    if(!fs.existsSync(`./archives/${path}.txt`)) return "File Not Found";
     return new Promise((resolve, reject) => {
         fs.readFile(`./archives/${path}.txt`, "utf-8", (error, data) =>{
             if (error) reject(error);
--- index.js
+++ index.js
@@ -12,15 +12,36 @@ fs.readdirSync('./commands').forEach(file =>{
     const commandName = file.split('.')[0];
     commands[commandName] = command;
 });
-botTelegram.on('message', msg => {
+botTelegram.on('message', async msg => {
     const chatId = msg.chat.id;
-    if(!msg.text.startsWith('/')) return;
-    const command = msg.text.split("/")[1].split(" ")[0];
-    console.log(command);
-    const mensaje = msg.text.split(" ")[1];
-    if(commands[command]){
-        commands[command](botTelegram, chatId, mensaje);
-    }else{
-        botTelegram.sendMessage(chatId,'Comando no reconocido.');
+    try {
+        if(!msg.text.startsWith('/')) return;
+        const command = msg.text.split("/")[1].split(" ")[0];
+        const mensaje = msg.text.split(" ")[1];
+        console.log(command);
+        const usuario = await getUser(msg.from.username);
+        console.log(msg.from.username);
+        if(!usuario){
+            await botTelegram.sendMessage(chatId, '🚫 No tienes permisos para usar el bot contacta a @Antonello54 !');
+            return;
+        }
+        if(commands[command]){
+            commands[command](botTelegram, chatId, mensaje);
+        }else{
+            await botTelegram.sendMessage(chatId,'⭕️ Comando no reconocido.');
+        }
+    } catch (error) {
+        console.log(error);
     }
 });
+
+const getUser = (user) => {
+    return new Promise((resolve, reject) =>{
+        fs.readFile(('./config/users.txt'), 'utf-8', (error, data) =>{
+            if(error) reject(error);
+            console.log(data);
+            const username = data.includes(user);
+            resolve(username);
+        });
+    });
+};
